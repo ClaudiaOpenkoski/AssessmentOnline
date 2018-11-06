4e4546b0affed32a8c1cd4dcf5c6d971020d59e9
ClaudiaOpenkoski <claudia.openkoski@gmail.com>
2018-11-06 17:37:11 +1300

    New Version

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { TestFrontendCategoryModule } from './category/category.module';
+import { TestFrontendCourseModule } from './course/course.module';
+import { TestFrontendModuleModule } from './module/module.module';
+import { TestFrontendTopicModule } from './topic/topic.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        TestFrontendCategoryModule,
+        TestFrontendCourseModule,
+        TestFrontendModuleModule,
+        TestFrontendTopicModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
